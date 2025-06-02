bc505382e539a2fdb7f5f45210edd2ffcad721b0
Md Limon Ali <limonali105@gmail.com>
2025-06-02 20:57:46 +0600

    update colors, icon-item section, icon-item section2, faq section, img-text section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,14 @@ $(document).ready(function () {
 		$(this).parent().siblings().removeClass('active').find('.product-accordion-item-content').slideUp();
 	});
 	// **..product-accordion js end..**
+
+	// **..faq-accordion js start..**
+	$('.faq-accordion-toggle-btn').on('click', function () {
+		$(this).next().slideToggle();
+		$(this).parent().toggleClass('active');
+		$(this).parent().siblings().removeClass('active').find('.faq-accordion-item-content').slideUp();
+	});
+	// **..faq-accordion js end..**
 })
 
 // product-slider js start---
